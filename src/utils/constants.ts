d08f0bb0dedcbe26abaab9a68fd98feb2ef64bb1
Darius <dpdarius1@gmail.com>
2024-04-27 21:05:28 +0300

    update utils

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
 export const GRID_COL_SIZES =
   "0.4fr 2fr 0.4fr 0.7fr 0.7fr 1fr 0.6fr 0.8fr 0.3fr";
-export const REFRESH_DATA_TIMEOUT = 800;
+export const REFRESH_DATA_TIMEOUT = 1000;
+export const SHOW_VALUES = [25, 50, 100];
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,8 +1,16 @@
 export function isFutureDate(inputDateString: string): boolean {
   const inputDate = new Date(inputDateString);
   const currentDate = new Date();
-  // Remove time part from the date as we are only comparing the date
   currentDate.setHours(0, 0, 0, 0);
   inputDate.setHours(0, 0, 0, 0);
   return inputDate > currentDate;
+}
+
+export function convertDateFormat(inputDate: string): string {
+  const date = new Date(inputDate);
+  const year = date.getFullYear();
+  const month = ("0" + (date.getMonth() + 1)).slice(-2);
+  const day = ("0" + date.getDate()).slice(-2);
+
+  return `${year}-${month}-${day}`;
 }
